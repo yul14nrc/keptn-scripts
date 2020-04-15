34a872c6ca395a9b1552780d8c8b713c1ad306cb
yul14nrc <61518345+yul14nrc@users.noreply.github.com>
2020-04-15 17:45:05 -0400

    Create setupDynatraceSLIService.sh

---
+++ scripts/setupDynatraceSLIService.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DT_TENANT=$(cat creds_dt.json | jq -r '.dynatraceTenant')
+DT_API_TOKEN=$(cat creds_dt.json | jq -r '.dynatraceApiToken')
+
+LATEST_RELEASE=$(curl -s https://api.github.com/repos/keptn-contrib/dynatrace-sli-service/releases/latest | grep tag_name | cut -d '"' -f 4)
+
+echo "============================================================="
+echo "About to install the Dynatrace SLI Service $LATEST_RELEASE for keptn to talk to $DT_TENANT"
+echo "============================================================="
+read -rsp $'Press ctrl-c to abort. Press any key to continue...\n' -n1 key
+
+## Install Dynatrace SLI Service
+git clone --branch release-$LATEST_RELEASE https://github.com/keptn-contrib/dynatrace-sli-service --single-branch
+kubectl apply -f dynatrace-sli-service/deploy/
