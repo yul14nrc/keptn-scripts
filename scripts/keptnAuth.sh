425bcaa2763e00e915a37aee2f563d62a62d9413
yul14nrc <61518345+yul14nrc@users.noreply.github.com>
2020-04-20 13:09:23 -0400

    Create keptnAuth.sh

---
+++ scripts/keptnAuth.sh
@@ -0,0 +1,9 @@
+echo "KEPTN_API_URL:"
+KEPTN_API_URL=https://api.keptn.$(kubectl get cm -n keptn keptn-domain -ojsonpath={.data.app_domain})
+echo $KEPTN_API_URL
+echo ""
+echo "KEPTN_API_TOKEN:"
+KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -ojsonpath={.data.keptn-api-token} | base64 --decode)
+echo $KEPTN_API_TOKEN
+echo ""
+keptn auth --endpoint=$KEPTN_API_URL --api-token=$KEPTN_API_TOKEN
